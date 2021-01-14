dbb6a118e7204b68f55528298e83c20a5c5f55bb
ChesterEucrine <60994375+ChesterEucrine@users.noreply.github.com>
2021-01-14 14:14:38 +0200

    Add files via upload

--- NonFlask/all.js
+++ NonFlask/all.js
@@ -1,3 +1,26 @@
-function() {
-  console.log(location.href);
+function main() {
+  var l = window.location.href;
+  var arr = l.split('/');
+  var current = arr[arr.length-1];
+
+  if (current == "index.html") {
+    home = document.getElementById('home');
+    home.className = 'selected';
+  } else if (current == "packages.html") {
+    packages = document.getElementById('packages');
+    packages.className = 'selected';
+  } else if (current == "contact.html") {
+    contact = document.getElementById('contact');
+    contact.className = 'selected';
+  } else if (current == "guide.html") {
+    guide = document.getElementById('guide');
+    guide.className = 'selected';
+  } else if (current == "team.html") {
+    team = document.getElementById('team');
+    team.className = 'selected';
+  } else if (current == "") {
+
+  }
 }
+
+main();
