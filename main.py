d82ace252a331de8f4bee1b8af866e57a3db8336
ChesterEucrine <blessingdlamini24747@gmail.com>
2021-01-19 11:48:04 +0200

    Created Flask version of OTC NonFlask version

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
-app.route("/")
-app.route("/home/")
+@app.route("/")
+@app.route("/home/")
 def home():
     return render_template("index.html")
+
+@app.route('/packages/')
+def packages():
+	return render_template("packages.html")
+
+@app.route('/guide/')
+def guide():
+	return render_template("guide.html")
+
+@app.route('/team/')
+def team():
+	return render_template("team.html")
+
+@app.route("/contact/")
+def contact():
+	return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
